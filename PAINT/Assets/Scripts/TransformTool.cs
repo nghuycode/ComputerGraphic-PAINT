8d28337b076fca40c4665521ff9fd7149ab3b979
Nguyen Gia Huy <nghuycode@gmail.com>
2021-05-02 14:20:04 +0700

    transform

---
+++ PAINT/Assets/Scripts/TransformTool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformTool : MonoBehaviour
+{
+    public Shape CurrentShape;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.L)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Plus)) 
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Minus)) 
+        {
+
+        }
+    }
+}
